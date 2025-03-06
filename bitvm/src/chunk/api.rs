9ec2bf89100b1eb6c4ba94eba868f4179e77322e
Andrew0ng <Andrew0ng@bitlayer.ltd>
2025-03-06 21:49:29 +0800

    change number of scripts

--- bitvm/src/chunk/api.rs
+++ bitvm/src/chunk/api.rs
@@ -17,9 +17,9 @@ use super::api_runtime_utils::{
 };
 use super::wrap_hasher::BLAKE3_HASH_LENGTH;
 
-pub const NUM_PUBS: usize = 1;
+pub const NUM_PUBS: usize = 5;
 pub const NUM_U256: usize = 14;
-pub const NUM_U160: usize = 363;
+pub const NUM_U160: usize = 379;
 pub const VALIDATING_TAPS: usize = 1;
 pub const HASHING_TAPS: usize = NUM_U160;
 pub const NUM_TAPS: usize = HASHING_TAPS + VALIDATING_TAPS;
