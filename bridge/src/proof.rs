50e42754f96e2f9e96b992a691fbe1873c22d808
Andrew0ng <Andrew0ng@bitlayer.ltd>
2025-03-24 11:40:32 +0800

    remove crypto primitive

--- bridge/src/proof.rs
+++ bridge/src/proof.rs
@@ -1,5 +1,4 @@
 use ark_bn254::{g1::G1Affine, Bn254};
-use ark_crypto_primitives::snark::{CircuitSpecificSetupSNARK, SNARK};
 use ark_ec::pairing::Pairing;
 use ark_ff::PrimeField;
 use ark_groth16::Groth16;
@@ -12,26 +11,7 @@ use rand::{RngCore, SeedableRng};
 
 // TODO: replace with actual implementation
 pub fn get_proof() -> RawProof {
-    type E = Bn254;
-    let k = 6;
-    let mut rng = ark_std::rand::rngs::StdRng::seed_from_u64(test_rng().next_u64());
-    let circuit = DummyCircuit::<<E as Pairing>::ScalarField> {
-        a: Some(<E as Pairing>::ScalarField::rand(&mut rng)),
-        b: Some(<E as Pairing>::ScalarField::rand(&mut rng)),
-        num_variables: 10,
-        num_constraints: 1 << k,
-    };
-    let (pk, vk) = Groth16::<E>::setup(circuit, &mut rng).unwrap();
-
-    let c = circuit.a.unwrap() * circuit.b.unwrap();
-
-    let proof = Groth16::<E>::prove(&pk, circuit, &mut rng).unwrap();
-
-    RawProof {
-        proof,
-        public: vec![c],
-        vk,
-    }
+    RawProof::default()
 }
 
 // DO NOT USE IN PRODUCTION! This is a test function.
